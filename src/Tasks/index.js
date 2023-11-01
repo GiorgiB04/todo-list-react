02aa2eaa216d46ce39628283a5d758cd453a0171
Giorgi Botchorishvili <giorgibochorishvili04@gmail.com>
2023-11-01 04:09:51 +0100

    creat todo list react

---
+++ src/Tasks/index.js
@@ -0,0 +1,21 @@
+import "./style.css";
+
+
+const Tasks = (props) => (
+  <ul className="tasks">
+    {props.tasks.map(task => (
+      <li
+      className={`task__item${task.done && props.hideDoneTasks ? " task__hidden" : ""}`}>
+        <button className="task__button js-done"> 
+          {task.done ? "✔" : ""}
+          </button>
+          <span className={`form__text ${task.done ? " task__decoration " : ""}`}>
+          {task.content}
+          </span>
+          <button className="remove__button">🗑</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default Tasks;
