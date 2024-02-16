b1dba12c126206112e32a3536b2edc13f29fee7f
Giorgi Botchorishvili <giorgibochorishvili04@gmail.com>
2024-02-16 02:39:36 +0100

    react dom changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
@@ -7,14 +7,14 @@ import { GlobalStyle } from './globalStyle';
 import * as serviceWorker from './serviceWorker';
 import { theme } from './theme';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
